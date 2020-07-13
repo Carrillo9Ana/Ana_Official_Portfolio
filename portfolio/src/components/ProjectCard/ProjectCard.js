f944733614fb3005411b993f28d354fe33832752
Ana Carrillo <acarrillo9724@gmail.com>
2020-07-13 12:16:23 -0700

    fixed typo in project card

--- portfolio/src/components/ProjectCard/ProjectCard.js
+++ portfolio/src/components/ProjectCard/ProjectCard.js
@@ -7,7 +7,7 @@ export const ProjectCard = ({ project }) => {
         <div className="project-card">
             <p><b>Name:</b> {project.name} </p><br/>
             <a href={project.link}><img className="images" src={project.image} alt=" project "/></a><br/>
-            <p><b>Describtion:</b> {project.description} </p>
+            <p><b>Description:</b> {project.description} </p>
             <br/>
             <p><a href={project.github}><img src="https://avatars3.githubusercontent.com/u/9919?s=40&v=4" alt="github"/></a></p>
         </div>
