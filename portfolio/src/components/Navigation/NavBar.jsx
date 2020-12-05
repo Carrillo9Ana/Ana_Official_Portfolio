6886ade88f596a0276f7de3344d9021e85598615
Ana Carrillo <acarrillo9724@gmail.com>
2020-12-04 22:34:37 -0500

    navigation

--- portfolio/src/components/Navigation/NavBar.jsx
+++ portfolio/src/components/Navigation/NavBar.jsx
@@ -9,7 +9,7 @@ export default function NavBar() {
   return (
     <>
       <Navbar bg="info" variant="dark">
-        <Navbar.Brand href="#home">{"{AC}"} Ana Carrillo</Navbar.Brand>
+        <Navbar.Brand href="#home">{"{AC}"} Carrillo, Ana K.</Navbar.Brand>
         <Nav className="mr-auto">
           <Nav.Link href="#home">Home</Nav.Link>
           <Nav.Link href="#features">Features</Nav.Link>
