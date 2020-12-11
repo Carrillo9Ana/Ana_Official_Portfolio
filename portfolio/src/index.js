ec4fd8d1aff7ece5562f0a3f048512946748e6f3
Ana Carrillo <acarrillo9724@gmail.com>
2020-12-11 18:37:42 -0500

    working on my profile landing page

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -2,21 +2,41 @@ import React from "react";
 import ReactDOM from "react-dom";
 import {
   BrowserRouter as Router,
-  Router,
-  userHisory,
+  Route,
+  useHistory,
   Switch,
 } from "react-router-dom";
-
+import { Security, LoginCallback, SecureRoute } from '@okta/okta-react';
 import "antd/dist/antd.css";
 
-import App from "./components/App";
+// routes/pages will go here in the future
+import { config } from './oktaConfig.js';
 
 ReactDOM.render(
   <Router>
     <React.StrictMode>
       <App />
     </React.StrictMode>
-    ,
   </Router>,
   document.getElementById("root")
 );
+
+function App() {
+  // The reason to declare App this way is so that we can use any helper functions we'd need for business logic, in our case auth.
+  // React Router has a nifty useHistory hook we can use at this level to ensure we have security around our routes.
+  const history = useHistory();
+
+  const authHandler = () => {
+    // We pass this to our <Security /> component that wraps our routes.
+    // It'll automatically check if userToken is available and push back to login if not :)
+    history.push('/login');
+  };
+  return (
+    <Security {...config} onAuthRequired={authHandler}>
+      <Switch>
+        <div>Hello</div>
+      </Switch>
+    </Security>
+  )
+}
+
